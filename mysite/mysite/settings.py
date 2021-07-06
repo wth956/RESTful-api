3f071637ba935d1b55e27cb2b243362351953902
Dylan Chen <wth956@gmail.com>
2021-07-06 19:30:54 +0800

    install MySQL

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# modify SQLite to MySQL
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysite',
+        'USER': 'root',
+        'PASSWORD': '0526',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',# mysql default
     }
 }
 
